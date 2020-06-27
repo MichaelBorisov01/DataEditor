2834a64d3e0c3b1d35f8c9fc0b3caf70955b5ca9
Михаил Борисов <47294947+MichaelBorisov01@users.noreply.github.com>
2020-06-27 14:21:01 +0300

    Add files via upload

---
+++ DataEditor/DataEditor/StringDataSource.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Xml.Serialization;
+using System.Text.RegularExpressions;
+
+namespace WpfApp3
+{
+    public class Student
+    {
+        public Student() { }
+        
+        public Student(string name,string group)
+        {
+            this.Name = name;
+            this.Group = group;
+        }
+
+        public string Name { get; set; }
+        public string Group {get; set;}
+    }
+
+public class Group
+    {
+    
+    public int Number { get; set; }
+    public int Cource { get; set; }
+    public string Track { get; set; }
+    }
+    [Serializable]
+    public class StringDataSource
+    {
+        public ObservableCollection<Student> data = new ObservableCollection<Student>();
+    }
+}
---
+++ DataEditor/DataEditor/Window1.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Microsoft.Win32;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace WpfApp3
+{
+    public partial class Window1 : Window
+    {
+
+        StringDataSource _dataSource;
+        public StringDataSource DataSource
+        {
+            get
+            {
+                return _dataSource;
+            }
+            set
+            {
+                _dataSource = value;
+                listBox.ItemsSource = value.data;
+            }
+        }
+
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        public Window1()
+        {
+            InitializeComponent();
+
+            saveFileDialog.DefaultExt = ".xml";
+            openFileDialog.DefaultExt = ".xml";
+            saveFileDialog.Filter = "XML Files|*.xml|All Files|*.*";
+            openFileDialog.Filter = "XML Files|*.xml|All Files|*.*";
+
+            DataSource = new StringDataSource();
+            //DataSource.data.Add(new Student("Borisov Michael Denisovich"));
+            //DataSource.data.Add(new Student("Matyash Daniil Denisovich"));
+
+        }
+       
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        { 
+            NewStudent newStudentDialog = new NewStudent();
+              
+            if (newStudentDialog.ShowDialog() == true)
+            {
+                DataSource.data.Add(newStudentDialog.Student);
+            }
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataSource.data.Remove(listBox.SelectedItem as Student);
+        }
+
+        private void SaveMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+
+                var serializer = new XmlSerializer(typeof(StringDataSource));
+
+                using (XmlWriter fs = XmlWriter.Create(saveFileDialog.FileName))
+                {
+
+                    serializer.Serialize(fs, DataSource);
+
+                    Debug.WriteLine("Объект сериализован");
+                }
+            }
+        }
+
+        private void OpenMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var serializer = new XmlSerializer(typeof(StringDataSource));
+                using (XmlReader fs = XmlReader.Create(openFileDialog.FileName))
+                {
+                    
+                    DataSource = serializer.Deserialize(fs) as StringDataSource;
+                }
+            }
+        }
+
+        private void NewMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            DataSource = new StringDataSource();
+        }
+    }
+}
